fd64ed81722b7af65d8e56c5ee3aa721f0710aaa
Amanvishwakarmabot <147085734+Amanvishwakarmabot@users.noreply.github.com>
2024-06-28 05:52:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,9 +15,9 @@ def is_enabled(value, default):
         return default
 
 #main variables
-API_ID = int(environ.get('API_ID', '12380656'))
-API_HASH = environ.get('API_HASH', 'd927c13beaaf5110f25c505b7c071273')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7091587168:AAGpX2rHu-DVYmXUdlxM3vZRnaLBrx8k3-c")
+API_ID = int(environ.get('API_ID', '20389440'))
+API_HASH = environ.get('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7091587168:AAGpX2rHu-DVYmXUdlxM3vZRnaLBrx8k3-c')
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5977931010').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/")
